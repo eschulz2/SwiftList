9ec8cc785cba271f7fe09ed405fa649070548fe3
studio three <eric@thirddegreefitness.com>
2015-12-31 12:29:47 -0600

    Initial Commit

---
+++ SwiftList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftList
+//
+//  Created by Studio Three  on 12/31/15.
+//  Copyright © 2015 Eric Schulz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
